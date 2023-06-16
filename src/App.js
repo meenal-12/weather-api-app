6242e6f06696c518afc1e24e1e6040372abbcae7
meenal-12 <60273758+meenal-12@users.noreply.github.com>
2023-06-16 11:30:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Temp from './weather/Temp.js';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Temp/>
+    </>
+  );
+}
+
+export default App;
